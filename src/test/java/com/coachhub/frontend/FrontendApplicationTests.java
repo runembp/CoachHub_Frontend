f121899e7b5328acae089859276df50f5a252ec8
runembp <58623004+runembp@users.noreply.github.com>
2021-05-09 11:41:07 +0200

    Initial commit

---
+++ src/test/java/com/coachhub/frontend/FrontendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.coachhub.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
